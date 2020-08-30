82d572a106f31e7802268d650aa6a369d3bfdb28
chenbingchang <940001047@qq.com>
2020-08-30 19:21:46 +0800

    构建脚步bug

--- build.sh
+++ build.sh
@@ -3,15 +3,15 @@ env=$1
 
 echo "构建开始，环境：${env}"
 
-if [ ${env} == 'dev' ]; then
-  # 开发
-elif [ ${env} == 'test' ]; then
-  # 测试
-elif [ ${env} == 'stage' ]; then
-  # 预发布
-elif [ ${env} == 'prod' ]; then
-  # 生产
-fi
+# if [ ${env} == 'dev' ]; then
+#   # 开发
+# elif [ ${env} == 'test' ]; then
+#   # 测试
+# elif [ ${env} == 'stage' ]; then
+#   # 预发布
+# elif [ ${env} == 'prod' ]; then
+#   # 生产
+# fi
 
 # 安装依赖
 npm install
