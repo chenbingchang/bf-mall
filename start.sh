f31fda0126d0ddf67e0d55e839edeffcd2eec324
chenbingchang <940001047@qq.com>
2020-09-01 20:46:20 +0800

    nginx配置bug; 部署脚本bug

--- start.sh
+++ start.sh
@@ -9,5 +9,5 @@ docker rm bf-mall
 docker rmi -f registry-vpc.cn-shenzhen.aliyuncs.com/chenbc/bf-mall:$env
 # 拉取新镜像
 docker pull registry-vpc.cn-shenzhen.aliyuncs.com/chenbc/bf-mall:$env
-# 运行新容器，指定端口，名称，后台运行
-docker run -itd -p 6000:5000 --name bf-mall registry-vpc.cn-shenzhen.aliyuncs.com/chenbc/bf-mall:$env
+# 运行新容器，指定端口（要和nginx中的端口对应），名称，后台运行
+docker run -itd -p 6000:80 --name bf-mall registry-vpc.cn-shenzhen.aliyuncs.com/chenbc/bf-mall:$env
