2d74e3cef00615998ecd84ee464709808b42707c
chenbingchang <940001047@qq.com>
2020-09-02 09:25:43 +0800

    修改部署端口

--- start.sh
+++ start.sh
@@ -10,4 +10,5 @@ docker rmi -f registry-vpc.cn-shenzhen.aliyuncs.com/chenbc/bf-mall:$env
 # 拉取新镜像
 docker pull registry-vpc.cn-shenzhen.aliyuncs.com/chenbc/bf-mall:$env
 # 运行新容器，指定端口（要和nginx中的端口对应），名称，后台运行
-docker run -itd -p 6000:80 --name bf-mall registry-vpc.cn-shenzhen.aliyuncs.com/chenbc/bf-mall:$env
+# 掉进了一个坑：6000端口在谷歌浏览器无法使用，参考:https://blog.csdn.net/yenange/article/details/82178056  https://www.iteye.com/blog/mj914-1624394
+docker run -itd -p 7000:80 --name bf-mall registry-vpc.cn-shenzhen.aliyuncs.com/chenbc/bf-mall:$env
