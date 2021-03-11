6288f33de03840e51e6656829fdbb372d671e1e7
chenbingchang <94001047@qq.com>
2021-03-11 15:20:38 +0800

    优化cicd

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
-# 脚本的第一个参数是环境
-env=$1
+# 脚本的第一个参数是项目名称
+app_name=$1
+# 脚本的第二个参数是环境
+env=$2
+
+# 判断环境变量是否存在
+if [ -z "$app_name" ]; then
+  echo "请指定项目名称"
+  exit 1
+fi
 
 # 判断环境变量是否存在
 if [ -z "$env" ]; then
@@ -7,26 +15,39 @@ if [ -z "$env" ]; then
   exit 1
 fi
 
-echo "构建开始，环境：${env}"
+echo "构建开始，项目：${app_name}，环境：${env}"
+
+# 部署服务器地址
+server_host=""
+# 部署后，外部访问的端口
+port="7000"
 
 if [ ${env} == 'dev' ]; then
   # 开发
   echo 开发
+  server_host="http://cbingc.com:5001"
+  port="7000"
 elif [ ${env} == 'test' ]; then
   # 测试
   echo 测试
+  server_host="http://cbingc.com:5001"
+  port="7000"
 elif [ ${env} == 'stage' ]; then
   # 预发布
   echo 预发布
+  server_host="http://cbingc.com:5001"
+  port="7000"
 elif [ ${env} == 'prod' ]; then
   # 生产
   echo 生产
+  server_host="http://cbingc.com:5001"
+  port="7000"
 fi
 
 # 根据环境使用不同的nginx配置
 cp -f ./nginx/conf.d/${env}.conf ./nginx/conf.d/app.conf
 
-echo $PATH
+echo node版本信息
 # 查看node版本
 node --version
 npm --version
@@ -44,14 +65,21 @@ npm rebuild node-sass
 # 打包
 npm run build:$env
 
+# docker仓库地址，这里用的是阿里云的容器镜像服务
+docker_repo_url=registry-vpc.cn-shenzhen.aliyuncs.com/chenbc
+# docker镜像名称，格式：项目名称:环境
+docker_image_name=$app_name:$env
+# 镜像完整地址
+docker_image_url=$docker_repo_url/$docker_image_name
+
 # 删除旧的镜像
-docker rmi -f registry-vpc.cn-shenzhen.aliyuncs.com/chenbc/bf-mall:$env
-# 构建docker镜像
-docker build -t registry-vpc.cn-shenzhen.aliyuncs.com/chenbc/bf-mall:$env .
+# docker rmi -f $docker_image_url
+# 构建docker镜像，注意后面有个"."
+docker build -t $docker_image_url .
 # 把镜像推到啊里云镜像仓库
-docker push registry-vpc.cn-shenzhen.aliyuncs.com/chenbc/bf-mall:$env
+docker push $docker_image_url
 
 # 触发部署服务器的脚本
 if [ "$TOKEN" ]; then
-  curl "http://cbingc.com:5001/trigger/bf-mall?token=${TOKEN}&env=${env}"
+  curl "${server_host}/hook/trigger?token=${TOKEN}&appName=${app_name}&env=${env}&port=${port}"
 fi
--- start.sh
+++ start.sh
@@ -1,14 +1,25 @@
+# 项目名称
+app_name=$1
 # 输入的环境变量
-env=$1
+env=$2
+# 外部访问端口
+port=$3
+
+# docker仓库地址，这里用的是阿里云的容器镜像服务
+docker_repo_url=registry-vpc.cn-shenzhen.aliyuncs.com/chenbc
+# docker镜像名称，格式：项目名称:环境
+docker_image_name=$app_name:$env
+# 镜像完整地址
+docker_image_url=$docker_repo_url/$docker_image_name
 
 # 停止容器
-docker stop bf-mall
+docker stop $app_name
 # 删除旧容器
-docker rm bf-mall
+docker rm $app_name
 # 强制删除旧镜像
-docker rmi -f registry-vpc.cn-shenzhen.aliyuncs.com/chenbc/bf-mall:$env
+docker rmi -f $docker_image_url
 # 拉取新镜像
-docker pull registry-vpc.cn-shenzhen.aliyuncs.com/chenbc/bf-mall:$env
+docker pull $docker_image_url
 # 运行新容器，指定端口（要和nginx中的端口对应），名称，后台运行
 # 掉进了一个坑：6000端口在谷歌浏览器无法使用，参考:https://blog.csdn.net/yenange/article/details/82178056  https://www.iteye.com/blog/mj914-1624394
-docker run -itd -p 7000:80 --name bf-mall registry-vpc.cn-shenzhen.aliyuncs.com/chenbc/bf-mall:$env
+docker run -itd -p $port:80 --name $app_name $docker_image_url
